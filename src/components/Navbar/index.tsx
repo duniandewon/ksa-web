b92f0e19b0e074508beb49b8c0fffa16c62506a9
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-12-28 03:09:44 +0700

    fix(navbar):Fix active menu item for sub-pages

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -36,15 +36,20 @@ const MENUS = [
 export function Navbar() {
   const pathname = usePathname();
 
+  const activePage =
+    pathname === "/"
+      ? "home"
+      : MENUS.find((menu) => menu.path.includes(pathname.split("/")[1]))?.id;
+
   return (
     <nav>
       <List
         variant="horizontal"
         data={MENUS}
-        render={({ label, path }) => (
+        render={({ label, path, id }) => (
           <Link
             href={path}
-            data-active={pathname === path ? "true" : null}
+            data-active={id === activePage ? "true" : null}
             className="inline-block px-3 py-2 rounded-lg uppercase text-primary hover:bg-accent hover:text-accent-foreground data-[active]:bg-accent data-[active]:text-accent-foreground"
           >
             {label}
